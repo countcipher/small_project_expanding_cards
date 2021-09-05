18bee27d72272d3419c66b80f73e7470f7151f72
Count Cipher <chrisharlow366@yahoo.com>
2021-09-05 11:29:55 -0400

    com

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const panels = document.querySelectorAll('.panel');
+const body =document.querySelector('body');
 
 panels.forEach((panel) => {
     panel.addEventListener('click', () => {
         removeActiveClasses();
         panel.classList.add('active');
     });
+});
+
+
+
+body.addEventListener('click', (e) => {
+    if(!e.target.classList.contains('glass')){
+        removeActiveClasses();
+        console.log('clicked');
+    }
 });
 
 function removeActiveClasses(){
